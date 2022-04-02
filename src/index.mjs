723ebf999a107f0b3655e0facbfffb6db0361fb0
anzoid <jpatihullah@gmail.com>
2022-04-02 12:05:10 +0700

    feat:server controller and new conrtoller and router

---
+++ src/index.mjs
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+
+class App {
+  static app;
+  constructor() {
+    this.app = express();
+    this.plugin();
+    this.routes();
+  }
+
+  routes() {
+    this.app.route("/api/v1/").get((req, res) => {
+      res.send("Welcome to server");
+    });
+  }
+
+  plugin() {
+    this.app.use(bodyParser.json());
+    this.app.use(morgan("dev"));
+    this.app.use(helmet());
+    this.app.use(cors());
+  }
+}
+
+const app = new App().app;
+const port = 3000;
+
+app.listen(port, () =>
+  console.info(`Server running at http://localhost:${port}`)
+);
